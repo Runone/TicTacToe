67bbe6704b5f9d56f270e1a0346c9e8393a639c8
Danny <ngo.danny11@gmail.com>
2018-12-12 14:09:35 -0800

    Dev environment setup

---
+++ client/components/app.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <div>Tic Tac Toe</div>
+    )
+  }
+
+
+}
+
+
+export default App;
